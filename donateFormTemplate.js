2a15d79e587fb06978453657e1e874d656f89362
extolloint <60484882+extolloint@users.noreply.github.com>
2020-01-30 14:57:56 -0500

    Create donateFormTemplate.js

---
+++ donateFormTemplate.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<link href="Content/Css/HostedDonation.css" rel="stylesheet" type="text/css">
+
+<head>
+<title>Donate to Extollo International</title>
+<link href="https://extolloint.github.io/bloomerang/HostedDonation.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="wrapper">
+<a href="https://bloomerang.co/donate-securely-bloomerang" target="_blank" class="secureDonationBanner">
+<img src="https://crm.bloomerang.co/Content/Images/SecureDonationBanner.png" alt="Secure donation via Bloomerang"></a>
+<div id="content" class="widgetContent"></div>
+
+<!--insert self-hosted code from Bloomerang donation forms between here...
+
+
+.....and here-->
+
+</div>
+</body>
+</html>
